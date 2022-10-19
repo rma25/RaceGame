24665e708e244589e4e9260af1d370fa39d1eabf
Rodo Andrade <rodo.andrade00111@gmail.com>
2022-10-19 18:56:13 -0400

    Saving progress
    Trying to update code to latest engine

---
+++ MileStone/Assets/Scripts/CarController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //GetComponent<Rigidbody>().centerOfMass = centerOfMass;        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+}
