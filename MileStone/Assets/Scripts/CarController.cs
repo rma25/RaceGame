b634bbedc3d824cf7799f25c45605f021e48b08b
Rodo Andrade <rodo.andrade00111@gmail.com>
2022-10-19 20:05:23 -0400

    Update

--- MileStone/Assets/Scripts/CarController.cs
+++ MileStone/Assets/Scripts/CarController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public GameObject car;        
 
     // Start is called before the first frame update
     public void Start()
-    {
-        //GetComponent<Rigidbody>().centerOfMass = centerOfMass;        
+    {        
+        this.car = GetComponent<GameObject>();        
+        //GetComponent<Rigidbody>().centerOfMass = centerOfMass;
     }
 
     // Update is called once per frame
     public void Update()
+    {
+        
+    }
+
+    public void OnKeyboardInput(GameObject car)
     {
         
     }
